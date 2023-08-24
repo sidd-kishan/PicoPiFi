57e74a088abad0b2d3e0fa44a58eeb0ae2e8186d
sidd-kishan <aayush.deo1994@gmail.com>
2023-08-24 23:42:10 +0530

    Compiling with LWIP_SINGLE_NETIF disabled

--- build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
+++ build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
@@ -37,3 +37,9 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/arm-none-eabi-objdump")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/mnt/f/pico-webserver/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cmake_install.cmake")
+
+endif()
+
